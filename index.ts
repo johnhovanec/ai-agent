6fd6cc50bc05a957f2b43f588d58363b92f09f15
jhovan1 <jahovanec@gmail.com>
2025-08-26 21:41:39 -0400

    Add memory of chats to file database

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { runLLM } from './src/llm'
+import { addMessages, getMessages } from './src/memory'
 const userMessage = process.argv[2]
 
 if (!userMessage) {
@@ -7,6 +8,17 @@ if (!userMessage) {
   process.exit(1)
 }
 
-const response = await runLLM({ userMessage })
+// Save new message in the chat history
+await addMessages([{role: "user", content: userMessage}])
+
+// Get all the message history to forward to LLM.
+const messages  = await getMessages();
+
+const response = await runLLM({
+  messages,
+})
+
+// We have to save the response from the LLM or else it won't show up in the memory.
+await addMessages([{role: 'assistant', content: response}]);
 
 console.log(response)
