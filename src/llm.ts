9c35795edea5bfb00667faf667a8efa952c3c406
jhovan1 <jahovanec@gmail.com>
2025-08-26 20:15:22 -0400

    Basic querying against model

--- src/llm.ts
+++ src/llm.ts
@@ -1,35 +1,14 @@
-import { zodFunction } from 'openai/helpers/zod'
-import { z } from 'zod'
-import type { AIMessage } from '../types'
-import { openai } from './ai'
-import { systemPrompt } from './systemPrompt'
+import { openai } from "./ai";
 
 export const runLLM = async ({
-  model = 'gpt-4o-mini',
-  messages,
-  temperature = 0.1,
-  tools,
-}: {
-  messages: AIMessage[]
-  temperature?: number
-  model?: string
-  tools?: { name: string; parameters: z.AnyZodObject }[]
-}) => {
-  const formattedTools = tools?.map((tool) => zodFunction(tool))
+  userMessage,
+} : { userMessage: string}) => {
   const response = await openai.chat.completions.create({
-    model,
+    model: 'gpt-4o-mini',
+    temperature: 0.1,
     messages: [
-      {
-        role: 'system',
-        content: systemPrompt,
-      },
-      ...messages,
-    ],
-    temperature,
-    tools: formattedTools,
-    tool_choice: 'auto',
-    parallel_tool_calls: false,
+      {role: "user", content: userMessage}
+    ]
   })
-
-  return response.choices[0].message
+  return response.choices[0].message.content;
 }
